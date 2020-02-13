6e19fbde5f99d00a713951d260cad19acab912c7
kTindalland <kaitindall@outlook.com>
2020-02-13 10:09:38 +0000

    Added GameState enum, proceeding onto making the xml class library

---
+++ code/Brightforest/Enums/GameState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brightforest.Enums
+{
+    public enum GameState
+    {
+        Loading,
+        Menu,
+        Exit,
+        Leaderboard,
+        PrePlay,
+        Play,
+        PostPlay
+    }
+}
